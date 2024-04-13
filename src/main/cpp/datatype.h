5c68444be46a545bacfd77cf7eb3fe7b7ead1c62
petrkucerak <petr@khome.cz>
2024-04-13 02:12:02 +0200

    Unify code formatting

--- src/main/cpp/datatype.h
+++ src/main/cpp/datatype.h
@@ -1,6 +1,6 @@
 #ifndef DATATYPE_H
 #define DATATYPE_H
 
-enum DataType {DOWNLOAD, UPLOAD, PING};
+enum DataType { DOWNLOAD, UPLOAD, PING };
 
 #endif // DATATYPE_H
--- src/main/cpp/measurementinfo.h
+++ src/main/cpp/measurementinfo.h
@@ -3,29 +3,29 @@
 
 #include <string>
 
-#include <json/json.h>
 #include "jsonserializable.h"
+#include <json/json.h>
 
 class MeasurementInfo : public JsonSerializable
 {
-private:
-    int id;
-    Json::Int64 timestamp;
-    std::string measurerName;
+   private:
+   int id;
+   Json::Int64 timestamp;
+   std::string measurerName;
 
-public:
-    MeasurementInfo();
-    MeasurementInfo(int id, long timestamp, std::string measurerName);
+   public:
+   MeasurementInfo();
+   MeasurementInfo(int id, long timestamp, std::string measurerName);
 
-    void Serialize( Json::Value& root );
-    void Deserialize( Json::Value& root );
+   void Serialize(Json::Value &root);
+   void Deserialize(Json::Value &root);
 
-    int getId() const;
-    void setId(int value);
-    long getTimestamp() const;
-    void setTimestamp(long value);
-    std::string getMeasurerName() const;
-    void setMeasurerName(const std::string &value);
+   int getId() const;
+   void setId(int value);
+   long getTimestamp() const;
+   void setTimestamp(long value);
+   std::string getMeasurerName() const;
+   void setMeasurerName(const std::string &value);
 };
 
 #endif // MEASUREMENTINFO_H
