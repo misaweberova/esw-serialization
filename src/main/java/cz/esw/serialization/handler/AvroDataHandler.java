7e1bc922aba675522dadbc686e8bfc3caefc05f1
misaweberova <123831342+misaweberova@users.noreply.github.com>
2024-04-14 21:24:06 +0200

    change avrodatahandler java impl

--- src/main/java/cz/esw/serialization/handler/AvroDataHandler.java
+++ src/main/java/cz/esw/serialization/handler/AvroDataHandler.java
@@ -1,9 +1,11 @@
 package cz.esw.serialization.handler;
 
 import cz.esw.serialization.ResultConsumer;
-import cz.esw.serialization.avro.ADataset;
+import cz.esw.serialization.avro.AAverage;
+import cz.esw.serialization.avro.ARecords;
+import cz.esw.serialization.avro.ARequestTuple;
+import cz.esw.serialization.avro.AResponseTuple;
 import cz.esw.serialization.avro.AMeasurementInfo;
-import cz.esw.serialization.avro.AResult;
 import cz.esw.serialization.json.DataType;
 import org.apache.avro.file.DataFileStream;
 import org.apache.avro.file.DataFileWriter;
@@ -11,6 +13,7 @@ import org.apache.avro.io.DatumReader;
 import org.apache.avro.io.DatumWriter;
 import org.apache.avro.specific.SpecificDatumReader;
 import org.apache.avro.specific.SpecificDatumWriter;
+
 
 import java.io.*;
 import java.nio.ByteBuffer;
@@ -23,7 +26,9 @@ public class AvroDataHandler implements DataHandler {
     private InputStream is;
     private OutputStream os;
 
-    protected Map<Integer, ADataset> datasets;
+    protected Map<Integer, ARecords> datasets;
+
+    protected List<AMeasurementInfo> measurementInfoList;
 
     public AvroDataHandler(InputStream is, OutputStream os) {
         this.is = is;
@@ -33,33 +38,46 @@ public class AvroDataHandler implements DataHandler {
     @Override
     public void initialize() {
         datasets = new HashMap<>();
+        measurementInfoList = new ArrayList<>();
     }
 
     @Override
     public void handleNewDataset(int datasetId, long timestamp, String measurerName) {
         AMeasurementInfo info = new AMeasurementInfo(datasetId, timestamp, measurerName);
-        Map<CharSequence, List<Double>> recordMap = new HashMap<>();
-        datasets.put(datasetId, new ADataset(info, recordMap));
+        measurementInfoList.add(info);
+        ARecords aRecords =  new ARecords(new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+        datasets.put(datasetId, aRecords);
     }
 
     @Override
     public void handleValue(int datasetId, DataType type, double value) {
-        ADataset dataset = datasets.get(datasetId);
+        ARecords dataset = datasets.get(datasetId);
         if (dataset == null) {
             throw new IllegalArgumentException("Dataset with id " + datasetId + " not initialized.");
         }
-        dataset.getRecords().computeIfAbsent(type.name(), t -> new ArrayList<>()).add(value);
+
+        if(type.equals(DataType.DOWNLOAD)) {
+            dataset.getDOWNLOAD().add(value);
+        }
+        else if(type.equals(DataType.PING)) {
+            dataset.getPING().add(value);
+        }
+        else {
+            dataset.getUPLOAD().add(value);
+        }
     }
 
     @Override
     public void getResults(ResultConsumer consumer) throws IOException {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        DatumWriter<ADataset> datumWriter = new SpecificDatumWriter<>(ADataset.class);
-        DataFileWriter<ADataset> fileWriter = new DataFileWriter<>(datumWriter);
+        DatumWriter<ARequestTuple> datumWriter = new SpecificDatumWriter<>(ARequestTuple.class);
+        DataFileWriter<ARequestTuple> fileWriter = new DataFileWriter<>(datumWriter);
         fileWriter.create(datasets.values().iterator().next().getSchema(), baos);
 
-        for (ADataset dataset : datasets.values()) {
-            fileWriter.append(dataset);
+        for(AMeasurementInfo measurementInfo : measurementInfoList) {
+            ARecords aRecords = datasets.get(measurementInfo.getId());
+            ARequestTuple requestTuple = new ARequestTuple(aRecords, measurementInfo);
+            fileWriter.append(requestTuple);
         }
 
         fileWriter.close();
@@ -75,18 +93,18 @@ public class AvroDataHandler implements DataHandler {
         os.write(dataBytes);
 
         // Receive response from server
-        DatumReader<AResult> resultsReader = new SpecificDatumReader<>(AResult.class);
-        DataFileStream<AResult> dataFileStream = new DataFileStream<>(is, resultsReader);
+        DatumReader<AResponseTuple> resultsReader = new SpecificDatumReader<>(AResponseTuple.class);
+        DataFileStream<AResponseTuple> dataFileStream = new DataFileStream<>(is, resultsReader);
 
         while (dataFileStream.hasNext()) {
-            AResult result = dataFileStream.next();
-            AMeasurementInfo info = result.getInfo();
+            AResponseTuple result = dataFileStream.next();
+            AMeasurementInfo info = result.getMeasurementInfo();
+            AAverage average = result.getAverage();
             consumer.acceptMeasurementInfo(info.getId(), info.getTimestamp(), info.getMeasurerName().toString());
-            result.getAverages().entrySet().stream().forEach(
-                    entry -> consumer.acceptResult(DataType.valueOf(entry.getKey().toString()), entry.getValue().getValue())
-            );
+            consumer.acceptResult(DataType.DOWNLOAD, average.getDOWNLOAD());
+            consumer.acceptResult(DataType.PING, average.getPING());
+            consumer.acceptResult(DataType.UPLOAD, average.getUPLOAD());
         }
         dataFileStream.close();
     }
-
 }
